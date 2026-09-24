350e5bb16ad779e40428808a47f1964e615c4bd8
CumulativeWebInc <hp@cumulativeweb.com>
2026-09-24 14:58:57 -0400

    Render Pandora artist + radio playlist links on artist pages

--- app.js
+++ app.js
@@ -323,6 +323,8 @@
     if (links.spotify_artist) linkRows.push(['Spotify artist page', links.spotify_artist]);
     if (links.apple_music_artist) linkRows.push(['Apple Music artist page', links.apple_music_artist]);
     if (links.deezer_artist) linkRows.push(['Deezer artist page', links.deezer_artist]);
+    if (links.pandora_artist) linkRows.push(['Pandora artist page', links.pandora_artist]);
+    if (links.radio_playlist) linkRows.push(['Artist radio playlist', links.radio_playlist]);
     if (links.ai_learning_set_playlist) linkRows.push(['AI Learning Set playlist', SPOT + '/playlist/' + links.ai_learning_set_playlist.split(':')[2]]);
     if (links.eric_alper_playlist) linkRows.push(['Zooted Zone on Eric Alper\u2019s "360° : The Best Indie Music"', SPOT + '/playlist/' + links.eric_alper_playlist.split(':')[2]]);
     if (links.podcast_rss) linkRows.push(['Catalog podcast RSS feed', links.podcast_rss]);
